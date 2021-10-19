20a9b5f744fd2791acca74bf89cbea3b60ead2cd
ilham B <80049379+Illeandra@users.noreply.github.com>
2021-10-20 01:06:15 +0700

    Update venom.js

--- venom.js
+++ venom.js
@@ -1,6 +1,6 @@
 alert('wkwkwkwkwkwkwkwkwkkwk');
 var p = document.getElementById('btn-berikan-penilaian');
 var trg = p.parentElement.querySelector('a.meta-item');
-settimeout( function(){
+setTimeout( function(){
   trg.click();
 }, 500);
