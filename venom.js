1cec8ed05ed1d1995c4848a7ea8fc751ebef3796
ilham B <80049379+Illeandra@users.noreply.github.com>
2021-11-02 12:10:38 +0700

    Delete venom.js

--- venom.js
+++
@@ -1,6 +0,0 @@
-alert('wkwkwkwkwkwkwkwkwkkwk');
-var p = document.getElementById('btn-berikan-penilaian');
-var trg = p.parentElement.querySelector('a.meta-item');
-setTimeout( function(){
-  trg.click();
-}, 500);
